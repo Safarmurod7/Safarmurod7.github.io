b001cb39da7c82853011cb866bb604f5c2fbf293
Ashurov Safarmurod <92220640+Safarmurod7@users.noreply.github.com>
2023-02-11 15:58:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-      window.onload = function() {
-      const snake = document.getElementById("snake");
-      let x = 0;
-      let y = 0;
-      let direction = "right";
-
-      setInterval(() => {
-        const segment = document.createElement("div");
-        segment.classList.add("snake-segment");
-        segment.style.left = `${x}px`;
-        segment.style.top = `${y}px`;
-        snake.appendChild(segment);
-
-        if (direction === "right") {
-          x += 50;
-        } else if (direction === "down") {
-          y += 50;
-        } else if (direction === "left") {
-          x -= 50;
-        } else if (direction === "up") {
-          y -= 50;
-        }
-
-        if (x > 450) {
-          direction = "down";
-        } else if (y > 150) {
-          direction = "left";
-        } else if (x < 0) {
-          direction = "up";
-        } else if (y < 0) {
-          direction = "right";
-        }
-      }, 500);
-      }
+window.onload = function() {
+const snake = document.getElementById("snake");
+let x = 0;
+let y = 0;
+let direction = "right";
+setInterval(() => {
+const segment = document.createElement("div");
+segment.classList.add("snake-segment");
+segment.style.left = `${x}px`;
+segment.style.top = `${y}px`;
+snake.appendChild(segment);
+if (direction === "right") {
+   x += 50;
+} else if (direction === "down") {
+  y += 50;
+} else if (direction === "left") {
+  x -= 50;
+} else if (direction === "up") {
+  y -= 50;
+}
+if (x > 450) {
+  direction = "down";
+} else if (y > 150) {
+  direction = "left";
+} else if (x < 0) {
+  direction = "up";
+} else if (y < 0) {
+  direction = "right";
+}
+}, 500);
+}
